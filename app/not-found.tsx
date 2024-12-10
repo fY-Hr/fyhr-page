395923290ca7e9ac7f41fbf70e2d25c562a7860f
Fakhry <anteikuwute@gmail.com>
2024-12-10 08:31:19 +0700

    feat: add welcome and 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound(){
+    return(
+        <>
+            <div className="flex justify-center items-center h-screen text-blue-900">
+                <div className="flex">
+                    <h1 className="text-2xl">404 | page cannot be found.</h1>
+                </div>
+            </div>
+        </>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
 import Image from "next/image";
+import Link from "next/link";
+import { HiArrowRight } from "react-icons/hi2"
+
 
 export default function Home() {
+
+  const welcome: string[] = ['W', 'e', 'l', 'c', 'o', 'm', 'e', '.'];
+
   return (
     <>
-      
+      <div 
+        className=" flex  flex-col items-center justify-center h-screen w-screen "
+      >
+
+        <div className="flex justify-center h-20">
+
+          {welcome.map((e: string, index: number) => (
+            <h1 key={index} className="text-5xl md:text-6xl select-none animate-pulse hover:animate-none transition-all hover:-translate-y-6 text-blue-900">{e}</h1>
+          ))}
+
+        </div>
+
+          <div className="flex gap-2 -mt-4 md:mt-2">
+            <Link href="/portofolios" className="text-blue-900 rounded-md px-2 py-1 bg-slate-50 flex gap-2 hover:bg-slate-200 transition-all duration-300 text-sm md:text-md md:px-4 md:py-2">see more<HiArrowRight className="self-center" /></Link>
+            <h1 className="select-none py-1 text-blue-900 text-sm md:text-md md:py-2">to my page</h1>
+          </div>
+        
+      </div>
     </>
   );
 }
