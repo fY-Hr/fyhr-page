8c12946a9ab5fa7a5fd3f81cfed6301dbb905583
Fakhry <anteikuwute@gmail.com>
2025-07-13 22:12:55 +0700

    fix: typo

--- app/experiences/PageContent.tsx
+++ app/experiences/PageContent.tsx
@@ -26,7 +26,7 @@ export default function PageContent(){
                             <h1 className="text-lg font-bold pl-2 pr-4 bg-gradient-to-r text-white bg-blue-900 ">2022</h1>
                         </div>
                         <div>
-                            <p className="self-center text-gray-600">Entering vocational highschool, majoring Software-Engineering.</p>
+                            <p className="self-center text-gray-600">Entering vocational high school, majoring Software-Engineering.</p>
                         </div>
                     </div>                
                     <div className="flex gap-4 mt-4 animate-fadeUpLast">
@@ -34,7 +34,7 @@ export default function PageContent(){
                             <h1 className="text-lg font-bold pl-2 pr-4 bg-gradient-to-r text-white bg-blue-900 whitespace-nowrap">2023 - 2024</h1>
                         </div>
                         <div>
-                            <p className="self-center text-gray-600"><span className="font-bold">Junior Programmer</span>&nbsp;| Internship at PT. Biggy Cemerlang.</p>
+                            <p className="self-center text-gray-600"><span className="font-bold">Junior Programmer</span>&nbsp;| Internship at <span className="underline">PT. Biggy Cemerlang</span>.</p>
                         </div>
                     </div>                
                     <div className="flex gap-4 mt-4 animate-fadeUpLast">
@@ -42,7 +42,7 @@ export default function PageContent(){
                             <h1 className="text-lg font-bold pl-2 pr-4 bg-gradient-to-r text-white bg-blue-900 ">2025</h1>
                         </div>
                         <div>
-                            <p className="self-center text-gray-600">Graduated from vocational highschool, majoring Software-Engineering.</p>
+                            <p className="self-center text-gray-600">Graduated from vocational high school, majoring Software-Engineering.</p>
                         </div>
                     </div>                
                     <div className="flex gap-4 mt-4 animate-fadeUpVeryLast">
@@ -50,7 +50,7 @@ export default function PageContent(){
                             <h1 className="text-lg font-bold pl-2 pr-4 bg-gradient-to-r text-white bg-blue-900 ">Present</h1>
                         </div>
                         <div>
-                            <p className="self-center text-gray-600"><span className="font-bold">IT Support</span>&nbsp;| at vocational highschool.</p>
+                            <p className="self-center text-gray-600"><span className="font-bold">IT Support</span>&nbsp;| at vocational high school.</p>
                         </div>
                     </div>          
                 </>
